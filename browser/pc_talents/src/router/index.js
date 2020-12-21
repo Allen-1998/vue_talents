77398a0f6a28210d1baab6edb4c652bcb81347a2
gzh <272970666@qq.com>
2020-12-21 16:54:09 +0800

    type:[新增]
    message:
    初始化vue项目

---
+++ browser/pc_talents/src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Register from "../views/Register.vue";
+import Login from "../views/Login.vue";
+import Index from "../views/Index.vue";
+import NotFound from "../views/404.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: "/", redirect: "/index" },
+  {
+    path: "/index",
+    name: "Index",
+    component: Index,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "*",
+    name: "Not Found",
+    component: NotFound,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
