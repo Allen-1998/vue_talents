84483cc3aa879a0c5e181a3388bc8ed139d2ad2a
gzh <272970666@qq.com>
2020-12-22 22:49:58 +0800

    type:[修改]
    message:
    1.修改接口返回值
    2.统一处理服务器错误

--- server/app/index.js
+++ server/app/index.js
@@ -1,14 +1,27 @@
 //入口文件
 const express = require("express");
+//post请求
 const bodyParser = require("body-parser");
+//跨域
+const cors = require("cors");
+//数据库
 const mongo = require("./config/db");
 const routers = require("./router/index");
 const app = new express();
 
 mongo(app);
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 routers(app);
+
+//处理错误请求
+app.use((err, req, res, next) => {
+  return res.json({
+    status: 500,
+    message: "服务器错误，请稍后再试！",
+  });
+});
 
 app.listen(3000, () => {
   console.log("server listen at localhost:3000...");
--- server/app/router/user.js
+++ server/app/router/user.js
@@ -13,50 +13,96 @@ code = (val) => {
 
 //验证身份中间件
 const rank = async (req, res, next) => {
-  //获取token
-  const token = req.headers.authorization.split(" ").pop();
-  const { _id, username } = jwt.verify(token, secret);
-  //查询用户是否存在
-  const user = await User.findById(_id);
-  if (!user) return res.status(422).send("token验证失败！");
-  if (username !== user.username)
-    return res.status(422).send("token验证失败！");
-  if (user.rank === 1) return next();
-  else return res.status(409).send("没有权限！");
+  try {
+    //获取token
+    const token = req.headers.authorization.split(" ").pop();
+    const { _id, username } = jwt.verify(token, secret);
+    //查询用户是否存在
+    const user = await User.findById(_id);
+    if (!user)
+      return res.json({
+        status: 422,
+        message: "token验证失败！",
+      });
+    if (username !== user.username)
+      return res.json({
+        status: 422,
+        message: "token验证失败！",
+      });
+    if (user.rank !== 1)
+      return res.json({
+        status: 409,
+        message: "没有权限！",
+      });
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 };
 
 //获取用户列表
 router.get("/", rank, async (req, res) => {
-  const list = await User.find();
-  res.send(list);
+  try {
+    const list = await User.find();
+    return res.send(list);
+  } catch (err) {
+    return next(err);
+  }
 });
 
 //注册
-router.post("/register", async (req, res) => {
-  //查询用户名是否已经存在
-  const user = await User.findOne({ username: req.body.username });
-  if (user) return res.status(409).send("该用户已存在！");
-  req.body.password = code(req.body.password);
-  //将用户信息能存储到数据库中
-  const newUser = await new User(req.body).save();
-  res.send(newUser);
+router.post("/register", async (req, res, next) => {
+  try {
+    //查询用户名是否已经存在
+    const user = await User.findOne({ username: req.body.username });
+    if (user)
+      return res.json({
+        status: 409,
+        message: "该用户已存在！",
+      });
+    req.body.password = code(req.body.password);
+    //将用户信息能存储到数据库中
+    await new User(req.body).save();
+    return res.json({
+      status: 200,
+      message: "注册成功！",
+    });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 //登录
-router.post("/login", async (req, res) => {
-  //查询用户是否存在
-  const user = await User.findOne({ username: req.body.username });
-  if (!user) return res.status(422).send("该用户不存在！");
-  //验证密码
-  if (code(req.body.password) !== user.password)
-    return res.status(422).send("密码错误！");
-  // 返回token
-  const { _id, username } = user;
-  const token = jwt.sign({ _id, username }, secret, { expiresIn: "24h" });
-  res.send(token);
+router.post("/login", async (req, res, next) => {
+  try {
+    //查询用户是否存在
+    const user = await User.findOne({ username: req.body.username });
+    if (!user)
+      return res.json({
+        status: 422,
+        message: "用户不存在！",
+      });
+    //验证密码
+    if (code(req.body.password) !== user.password)
+      return res.json({
+        status: 422,
+        message: "密码错误！",
+      });
+    // 返回token
+    const { _id, username } = user;
+    const token = jwt.sign({ _id, username }, secret, { expiresIn: "24h" });
+    return res.json({
+      status: 200,
+      message: "登录成功！",
+      token: token,
+      data: user,
+    });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 //验证
-router.post("/verify", async (req, res) => {});
+router.post("/verify", async (req, res, next) => {});
 
 module.exports = router;
